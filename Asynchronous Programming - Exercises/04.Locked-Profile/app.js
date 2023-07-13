49c61900e50cbde6fd44447ab8023207450ce2b4
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-07-13 21:18:15 +0300

    new tasks

--- Asynchronous Programming - Exercises/04.Locked-Profile/app.js
+++ Asynchronous Programming - Exercises/04.Locked-Profile/app.js
@@ -1,3 +1,9 @@
 function lockedProfile() {
-    console.log('TODO...')
+  //   e.preventDefault();
+  let userNameTextArea = document.getElementsByName("user1Username")[0];
+  //   userNameTextArea.value = "asdasdasd";
+  let main = document.getElementById("main");
+  let profile = document.querySelector(".profile");
 }
+
+function createUser(params) {}
